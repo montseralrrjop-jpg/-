06f339b4d014d41b2b76fd367e33d23178b342fe
Z User <z@container>
2026-05-11 18:02:48 +0000

    fix: add missing ViewGroup import + upgrade AGP 7.4.2 + Gradle 7.5

--- app/src/main/java/com/palestine/roots/ui/SiteAdapter.kt
+++ app/src/main/java/com/palestine/roots/ui/SiteAdapter.kt
@@ -2,6 +2,7 @@ package com.palestine.roots.ui
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.HapticFeedbackConstants
 import android.widget.ImageView
 import android.widget.TextView
